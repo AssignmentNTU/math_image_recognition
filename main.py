eacda16c1859911d347fb1809f4332b6e2eeab6f
Edward Sujono <sujonoe@garena.com>
2017-05-04 20:49:44 +0800

    add neural network

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from common.ImageSegmentor import Segmentor
 from common.GradientImageSegmentor import ImageSegmentor
+from traning.neural_network import NeuralNetwork
 
 if __name__ == "__main__":
 	# first segmenting all the image first
 	# file_name_path = "img/test2.jpg"
 	# segmentor = Segmentor()
 	# segmentor.start_segmenting_image(file_image_part=file_name_path)
-	image_segmentor = ImageSegmentor("img/test1.jpg")
-	image_segmentor.start_dsu_image_segmenting()
+	# image_segmentor = ImageSegmentor("img/test1.jpg")
+	# image_segmentor.start_dsu_image_segmenting()
+
+	neural_network = NeuralNetwork()
+	neural_network.start_training()
--- traning/neural_network.py
+++ traning/neural_network.py
@@ -1,49 +1,73 @@
 import tensorflow as tf
-
+# using tensorflow data example
+from tensorflow.examples.tutorials.mnist import input_data
 
 class NeuralNetwork:
 
-    number_node_1 = 500
-    number_node_2 = 500
-    number_node_3 = 500
-    output_classes = 10
-    batch_size = 100
+	number_node_1 = 500
+	number_node_2 = 500
+	number_node_3 = 500
+	output_classes = 10
+	batch_size = 100
 
-    def __init__(self):
-        # basically the ide
-        self.hidden_layer_1 = {
-            "weight": tf.Variable(tf.RandomNormal([784, self.number_node_1])),
-            "biases": tf.Variable(tf.RandomNormal([self.number_node_1]))
-        }
+	def __init__(self):
+		# basically the ide
+		self.hidden_layer_1 = {
+			"weight": tf.Variable(tf.RandomNormal([784, self.number_node_1])),
+			"biases": tf.Variable(tf.RandomNormal([self.number_node_1]))
+		}
 
-        self.hidden_layer_2 = {
-            "weight": tf.Variable(tf.RandomNormal([self.number_node_1, self.number_node_2])),
-            "biases": tf.Variable(tf.RandomNormal([self.number_node_2]))
-        }
+		self.hidden_layer_2 = {
+			"weight": tf.Variable(tf.RandomNormal([self.number_node_1, self.number_node_2])),
+			"biases": tf.Variable(tf.RandomNormal([self.number_node_2]))
+		}
 
-        self.hidden_layer_3 = {
-            "weight": tf.Variable(tf.RandomNormal([self.number_node_2, self.number_node_3])),
-            "biases": tf.Variable(tf.RandomNormal([self.number_node_3]))
-        }
+		self.hidden_layer_3 = {
+			"weight": tf.Variable(tf.RandomNormal([self.number_node_2, self.number_node_3])),
+			"biases": tf.Variable(tf.RandomNormal([self.number_node_3]))
+		}
 
-        self.output_layer = {
-            "weight": tf.Variable(tf.RandomNormal([self.number_node_3, self.output_classes])),
-            "biases": tf.Variable(tf.RandomNormal([self.output_classes]))
-        }
+		self.output_layer = {
+			"weight": tf.Variable(tf.RandomNormal([self.number_node_3, self.output_classes])),
+			"biases": tf.Variable(tf.RandomNormal([self.output_classes]))
+		}
 
-    def neural_network_model(self, data):
-        # activation function essentially just  x = 0 if x < 0 else x
-        layer_1 = tf.add(tf.matmul(data, self.hidden_layer_1["weight"]), self.hidden_layer_1["biases"])
-        layer_1 = tf.nn.relu(layer_1)
+		self.x = tf.placeholder('float', [None, 784])
+		self.y = tf.placeholder('float')
 
-        layer_2 = tf.add(tf.matmul(layer_1, self.hidden_layer_2["weight"]), self.hidden_layer_2["biases"])
-        layer_2 = tf.nn.relu(layer_2)
+	def neural_network_model(self):
+		data = self.x
+		# activation function essentially just  x = 0 if x < 0 else x
+		layer_1 = tf.add(tf.matmul(data, self.hidden_layer_1["weight"]), self.hidden_layer_1["biases"])
+		layer_1 = tf.nn.relu(layer_1)
 
-        layer_3 = tf.add(tf.matmul(layer_2, self.hidden_layer_3["weight"]), self.hidden_layer_3["biases"])
-        layer_3 = tf.nn.relu(layer_3)
+		layer_2 = tf.add(tf.matmul(layer_1, self.hidden_layer_2["weight"]), self.hidden_layer_2["biases"])
+		layer_2 = tf.nn.relu(layer_2)
 
-        output_layer = tf.add(tf.matmul(layer_3, self.output_layer["weight"]), self.output_layer["biases"])
+		layer_3 = tf.add(tf.matmul(layer_2, self.hidden_layer_3["weight"]), self.hidden_layer_3["biases"])
+		layer_3 = tf.nn.relu(layer_3)
 
-        return output_layer
+		output_layer = tf.add(tf.matmul(layer_3, self.output_layer["weight"]), self.output_layer["biases"])
 
-    def start_training(self):
+		return output_layer
+
+	def start_training(self):
+		prediction = self.neural_network_model()
+		cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction))
+		optimizer = tf.train.AdamOptimizer().minimize(cost)
+		mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+		hm_epoc = 10
+		# how many time we want to reloop the training
+		with tf.Session() as sess:
+
+			sess.run(tf.initialize_all_variables())
+
+			for epoch in range(hm_epoc):
+				loss_epoch = 0
+				for _ in range(int(mnist.train.num_examples/self.batch_size)):
+					epoch_x, epoch_y = mnist.train.next_batch(self.batch_size)
+					_, c = sess.run([optimizer, cost], feed_dict={self.x: epoch_x, self.y: epoch_y})
+					loss_epoch += c
+				correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y, 1))
+				accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+				print ("Epoch: %d Loss Epoch: %d, Accurracy: %f" % (epoch, loss_epoch, accuracy))
